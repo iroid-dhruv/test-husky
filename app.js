18cf2f36aed4c56e26760e238e11a849b9741ce9
Dhruv <dhruv.patel@iroidsolutions.com>
2024-06-04 18:44:51 +0530

    feat: implement husky, eslint

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line no-undef
 const app = require("express")();
 
 app.get("/", (req, res) => {
   return res.send("Hello World!");
 });
+
+const arr = [1, 2, 3];
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
